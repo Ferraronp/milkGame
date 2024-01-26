91a29f135a02d5a1a21c7c9ee87c47a2a16b424b
Ferraronp <avzal-ar@mail.ru>
2024-01-26 18:09:00 +0300

    fix bug with files

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
 from values import *
+
+try:
+    file_path = 'gamedata.txt'
+    open(file_path, 'x')
+except FileExistsError:
+    print('File already exists')
+try:
+    file_path = 'settings.txt'
+    open(file_path, 'x')
+except FileExistsError:
+    print('File already exists')
+
 from windows.game import show_game_window
 
 if __name__ == '__main__':
     pygame.init()
     pygame.font.init()
     pygame.mixer.init()
-
-    try:
-        file_path = 'gamedata.txt'
-        open(file_path, 'x')
-    except FileExistsError:
-        print('File already exists')
-    try:
-        file_path = 'settings.txt'
-        open(file_path, 'x')
-    except FileExistsError:
-        print('File already exists')
 
     try:
         show_game_window()
